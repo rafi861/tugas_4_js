964ca221ef289a5cc8ba667ec532e01e909ce4c3
Muhammad Rafi <muhammadrafi2545@gmail.com>
2023-01-20 17:32:09 +0700

    tugas 4 js

---
+++ tugas4.js
@@ -0,0 +1,18 @@
+var aku = 150;
+var kamu = 160;
+var dia = 170;
+
+if (170 > 160) {
+  console.log("tertinggi");
+}else {
+  console.log('tidak');
+}
+ if (150 > 160) {
+  console.log('terpendek');
+}else {
+  console.log('tinggi');
+}if (150 < 160) {
+  console.log('terpendek');
+}else {
+  console.log('tidak');
+}
